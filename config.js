959a1d39e446f123e535dcfab5760cdbf66e2ec2
Shengbo-1618 <shengbo1618@gmail.com>
2015-01-19 20:21:37 +0100

    added SQL injection fix, check undefined accountID issue per favore

---
+++ config.js
@@ -0,0 +1,7 @@
+/*jslint node: true */
+"use strict";
+
+exports.xmpp_user = "someuser@pvp.net/xiff";
+exports.xmpp_password = "AIR_somepass";
+exports.rtmp_user = "someuser";
+exports.rtmp_password = "somepass"; 
