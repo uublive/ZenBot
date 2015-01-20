a5f20f2687cfa05b86b817a9bb42a8af10bcf8ba
Matteo Alvazzi <matteo.alvazzi@gmail.com>
2015-01-20 22:09:37 +0100

    asd
    Signed-off-by: Matteo Alvazzi <matteo.alvazzi@gmail.com>

---
+++ dev_config.js
@@ -0,0 +1,7 @@
+/*jslint node: true */
+"use strict";
+
+exports.xmpp_user = "SinRival@pvp.net/xiff";
+exports.xmpp_password = "AIR_zenlol123";
+exports.rtmp_user = "sinrival";
+exports.rtmp_password = "zenlol123"; 
